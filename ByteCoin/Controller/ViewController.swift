7dff9c41098f261840c3b369d6ca0b4e42367c21
mstwrt <mstwrtgames@gmail.com>
2021-04-01 19:50:38 -0600

    final really done

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Angela Yu on 11/09/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
+//
 
 import UIKit
 
@@ -16,7 +17,7 @@ class ViewController: UIViewController {
     
     
     var coinManager = CoinManager()
-    
+    var mybitcoin = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
